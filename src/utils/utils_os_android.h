543b8185ff2f2819fbdde9106aec47b7a4e69d96
Emeric <emeric.grange@gmail.com>
2022-04-04 20:08:29 +0200

    wooops

--- src/utils/utils_os_android.h
+++ src/utils/utils_os_android.h
@@ -88,12 +88,12 @@ public:
     static bool getPermission_location_ble();
 
     /*!
-     * \return True if ACCESS_BACKGROUD_LOCATION permission has been previously obtained (for Android 10+).
+     * \return True if ACCESS_BACKGROUND_LOCATION permission has been previously obtained (for Android 10+).
      */
     static bool checkPermission_location_background();
 
     /*!
-     * \return True if ACCESS_BACKGROUD_LOCATION permission has been explicitly obtained (for Android 10+).
+     * \return True if ACCESS_BACKGROUND_LOCATION permission has been explicitly obtained (for Android 10+).
      */
     static bool getPermission_location_background();
 
